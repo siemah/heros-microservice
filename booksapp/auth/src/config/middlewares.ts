7d06cbcfcd9c873d455322cd998e1f8c7a71ca77
siemah <m_hamiche@inptic.edu.dz>
2019-08-01 15:50:00 +0100

    parse data send by users

--- booksapp/auth/src/config/middlewares.ts
+++ booksapp/auth/src/config/middlewares.ts
@@ -1,9 +1,11 @@
-import { Application } from "express";
+import { Application, json, urlencoded } from "express";
 import helmet from 'helmet';
 import compression from 'compression';
 import morgan from 'morgan';
 
 export default function setupMiddlewares (app: Application) {
+    app.use(json());
+    app.use(urlencoded({ extended: true }));
     app.use(morgan("dev"))
     app.use(compression());
     app.use(helmet());
