6d25601a8c6f68eb4a940ee02eb98287b853b3f8
siemah <m_hamiche@inptic.edu.dz>
2019-08-01 07:14:34 +0100

    update the doc of connect2db

--- booksapp/books/src/config/db.ts
+++ booksapp/books/src/config/db.ts
@@ -1,10 +1,16 @@
 import mongoose from "mongoose";
 
-// connextion to DB
+/**
+ * connextion to DB
+ * @author siemah
+ * @version 1.0.0
+ * @see mongoose docs
+ * @return Promise<string|any>
+ */
 const connect2db = (): Promise<string|any> => {
   return new Promise((resolve, reject) => {
     mongoose.connect(
-  "mongodb+srv://root:root@cluster0-igjak.mongodb.net/books?retryWrites=true&w=majority", //"mongodb://localhost:27017/customersdb",//"mongodb://localhost:27017/bookdb",
+      "mongodb+srv://root:root@cluster0-igjak.mongodb.net/books?retryWrites=true&w=majority", //"mongodb://localhost:27017/customersdb",//"mongodb://localhost:27017/bookdb",
       { useNewUrlParser: true },
       (err: any) => {
         if (err) reject( new Error(err));
