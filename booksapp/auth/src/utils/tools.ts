efedd00432292a3546f798e7ec0fc6ed8fef1bed
siemah <m_hamiche@inptic.edu.dz>
2019-08-02 14:33:15 +0100

    generate salt function

--- booksapp/auth/src/utils/tools.ts
+++ booksapp/auth/src/utils/tools.ts
@@ -1,10 +1,14 @@
 import jwt, { Secret, SignOptions } from 'jsonwebtoken';
-
+import crypto from 'crypto';
+import { GenerateSalt, } from '../types/ToolsTypes';
 /**
- * generate a JWT token for users
  * @author siemah   
  * @version 1.0.0
  * @see jsonwebtoken
+ */
+
+/**
+ * generate a JWT token for users
  * @param payload object present a data to send in JWT
  * @param secret Secret the secret key 
  * @param options SignOptions list of options 
@@ -23,4 +27,15 @@ export const generateJWTToken = (payload: object, secret:Secret, options: SignOp
     } catch (error) {
         reject(error.message);
     }
+});
+
+/**
+ * generate a salt
+ * @see crypto native node module
+ * @param length number represent the size of salt
+ */
+export const genSalt: GenerateSalt = (length:number) => new Promise((resolve, reject) => {
+    if(length <1) reject('Length must be more than or equal to 1')
+    let salt: string = crypto.randomBytes(length).toString('hex');
+    resolve(salt);
 });
