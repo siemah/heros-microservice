dee76b338f4e643560dade6e5fadd143d71a2dd6
siemah <m_hamiche@inptic.edu.dz>
2019-08-04 14:02:33 +0100

    add some routes for verify tokens and check roles and use the last one as permission checking for some routes

--- booksapp/auth/src/config/routes.ts
+++ booksapp/auth/src/config/routes.ts
@@ -1,5 +1,6 @@
 import { Application } from "express";
 import UserController from "../controllers/UserController";
+import { verifyToken } from "../utils/tools";
 /**
  * handle all auth routes
  * @author siemah
@@ -8,6 +9,16 @@ import UserController from "../controllers/UserController";
  */
 export function routesHandler(app: Application) {
     app.get('/auth/accounts', UserController.getAllUserAccount);
-    app.post('/auth/login', UserController.login);
-    app.post('/auth/create', UserController.createUserAccount);
+    app.post('/auth/verifytoken', async (req: any, res: any) => {
+        try {
+            let _res = await verifyToken(req.headers);
+            res.status(_res.status).json(_res.results);      
+        } catch (error) {
+            console.log(error)
+            res.status(error.status).json(error);
+        }
+    });
+    app.post('/auth/verifyRoles', UserController.getUserRoles);
+    app.post('/auth/login', UserController.login);// login or regenerate a new token
+    app.post('/auth/create', UserController.createUserAccount);// create an new user account 'customer'
 }
