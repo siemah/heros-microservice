582e11c73ee90e246b9d6f82aa771a23a2393c2e
siemah <m_hamiche@inptic.edu.dz>
2019-09-25 14:53:14 +0100

    Home & Login page

---
+++ booksapp/web app/src/shared/components/pages/Login.js
@@ -0,0 +1,36 @@
+import React, { useState, } from 'react';
+
+const Login = () => {
+  
+  const [credentials, setCredentials] = useState({
+    email: null,
+    password: null,
+  });
+  /**
+   * change the state depend on html form element
+   * @param {HTMLElement} target represent html form node element
+   */
+  const _onchange = ({ target }) => setCredentials({
+    ...credentials,
+    [target.name]: target.value,
+  });
+
+  return (
+    <form action='#' method='post'>
+      <div>
+        <label>
+          Email
+        </label><br />
+          <input name='email' onChange={_onchange} />
+      </div>
+      <div>
+        <label>
+          Password
+        </label><br />
+          <input name='password' type='password' onChange={_onchange} />
+      </div>
+    </form>
+  )
+}
+
+export default Login
