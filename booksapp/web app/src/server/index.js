0fb75d0d144d7277088c5c0ce3e599f77fb1a6c7
siemah <m_hamiche@inptic.edu.dz>
2019-09-25 14:24:40 +0100

    render JSX from server

--- booksapp/web app/src/server/index.js
+++ booksapp/web app/src/server/index.js
@@ -1,11 +1,15 @@
 import express from 'express';
+import React from 'react'
+import { renderToString, } from 'react-dom/server';
+import App from '../shared/App'
 
 let app = express();
 
 app.use(express.static('/public'));
 
 app.get('/*', (req, res) => {
-  res.send('hey there')
+  let _markup = renderToString(<App />);
+  res.send(_markup)
 });
 
 app.listen(3000, () => {
