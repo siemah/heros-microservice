83aed759d841be638d819db681cd2fbe4f8c8ea5
siemah <m_hamiche@inptic.edu.dz>
2019-09-25 19:43:13 +0100

    middleware 2 parse cookies

---
+++ booksapp/web app/src/server/middlewares/cookies.js
@@ -0,0 +1,20 @@
+/**
+ * parse cookies list to an object used 
+ * as middleware for expressjs app
+ * @param {Application.Request} req request details
+ * @param {Application.Response} res response object
+ * @param {Application.NextFunction} next next funtion
+ * @see expressjs 
+ */
+export const cookiesParser = function (req, res, next) {
+  const cookies = req.get('cookie');
+  if(!!cookies) 
+    req.cookies = cookies
+      .split(';')
+      .reduce((acc, e) => {
+        let trimedVal = e.trim();
+        let [key, val] = trimedVal.split('=');
+        return {...acc, [key]: val};
+      },{})
+  next();
+}
