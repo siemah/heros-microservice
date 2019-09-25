4b518f877996147a944b3006c1cac2abc7a6e7d7
siemah <m_hamiche@inptic.edu.dz>
2019-09-25 14:55:38 +0100

    render router in server & client sides

--- booksapp/web app/src/client/index.js
+++ booksapp/web app/src/client/index.js
@@ -1,8 +1,11 @@
-import React from 'react'
-import { hydrate } from 'react-dom'
-import App from '../shared/App'
+import React from 'react';
+import { hydrate } from 'react-dom';
+import App from '../shared/components/App';
+import { BrowserRouter as Router } from 'react-router-dom'
 
 hydrate(
-  <App />,
+  <Router>
+    <App />
+  </Router>,
   document.getElementById('app')
 );
--- booksapp/web app/src/server/index.js
+++ booksapp/web app/src/server/index.js
@@ -1,17 +1,22 @@
 import express from 'express';
-import React from 'react'
+import React from 'react';
 import { renderToString, } from 'react-dom/server';
-import App from '../shared/App'
+import App from '../shared/components/App';
+import { StaticRouter, } from 'react-router-dom'
 
 let app = express();
 
 app.use(express.static('/public'));
 
 app.get('/*', (req, res) => {
-  let _markup = renderToString(<App />);
-  res.send(_markup)
+  let _markup = renderToString(
+    <StaticRouter>
+      <App />
+    </StaticRouter>
+  );
+  res.send(_markup);
 });
 
 app.listen(3000, () => {
-  console.log('listening on port 3000')
-})
+  console.log('Follow http://localhost:3000');
+});
