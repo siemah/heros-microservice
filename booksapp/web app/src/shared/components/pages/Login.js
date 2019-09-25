d6ebe4c0f19120ab7a49d6e5dd5480a9c30b999a
siemah <m_hamiche@inptic.edu.dz>
2019-09-25 19:39:44 +0100

    handle login at client side

--- booksapp/web app/src/shared/components/pages/Login.js
+++ booksapp/web app/src/shared/components/pages/Login.js
@@ -1,11 +1,15 @@
 import React, { useState, } from 'react';
 import SEO from '../widgets/SEO';
 
-const Login = () => {
+const Login = ({ postLogin, }) => {
   
   const [credentials, setCredentials] = useState({
     email: null,
     password: null,
+  });
+  const [loginstate, setLoginstate] = useState({
+    loading: false,
+    message: null,
   });
   /**
    * change the state depend on html form element
@@ -15,10 +19,35 @@ const Login = () => {
     ...credentials,
     [target.name]: target.value,
   });
+  /**
+   * handle submiting login form
+   * @param {GlobalEventHandlers} evn event of submiting form
+   */
+  const _onsubmit = evn => {
+    evn.preventDefault();
+    setLoginstate({ loading: true });
+    postLogin(evn.target.action, credentials)
+      .then(res => {
+        if( res.message )
+          setLoginstate({ loading: false, message: res.message})
+        else if ( res.user ) {
+          setLoginstate({ loading: false, message: null, });
+          // update global state if its context or ..
+        } 
+        else 
+          setLoginstate({ loading: false, message: 'Something went wrong, try again', });
+      })
+      .catch(e => {
+        console.log('error', e)
+        setLoginstate({ loading: false, message: 'Something went wrong, try again' })
+      });
+  }
 
   return (
-    <form action='#' method='post'>
+    <form action='/auth/login' method='post' onSubmit={_onsubmit}>
       <SEO title='Login To Booksapp Account' />
+      <h1>Login</h1>
+      { loginstate.message && <mark>{loginstate.message}</mark>}
       <div>
         <label>
           Email
@@ -31,6 +60,7 @@ const Login = () => {
         </label><br />
           <input name='password' type='password' onChange={_onchange} />
       </div>
+      <button type='submit'>Let me in</button>
     </form>
   )
 }
