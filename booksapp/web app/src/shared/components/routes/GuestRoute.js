3c8d89fee567f7dc6e44964c1888b4a81248b6d9
siemah <m_hamiche@inptic.edu.dz>
2019-09-25 14:53:44 +0100

    Private & Guest routes

---
+++ booksapp/web app/src/shared/components/routes/GuestRoute.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Route, Redirect, } from 'react-router-dom';
+
+const GuestRoute = ({ isAthenticated = false, component: C, rest }) => {
+  return (
+    <Route {...rest} render={
+      props =>
+        !isAthenticated
+          ? <C {...rest} {...props} />
+          : <Redirect to='/login' />
+    } />
+  )
+}
+
+export default GuestRoute;
---
+++ booksapp/web app/src/shared/components/routes/PrivateRoute.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Route, Redirect, } from 'react-router-dom';
+
+const PrivateRoute = ({ isAthenticated = false, component: C, rest }) => {
+  return (
+    <Route {...rest} render={
+      props =>
+        isAthenticated
+          ? <C {...rest} {...props} />
+          : <Redirect to='/login' />
+    } />
+  )
+}
+
+export default PrivateRoute;
