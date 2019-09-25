d2d77849731361cf6c2fc2143a16a17b591f3bd6
siemah <m_hamiche@inptic.edu.dz>
2019-09-25 19:43:34 +0100

    implement cookie parser for server side

--- booksapp/web app/src/server/middlewares/index.js
+++ booksapp/web app/src/server/middlewares/index.js
@@ -1,4 +1,5 @@
 import authRouter from "../routes/auth";
+import { cookiesParser } from './cookies'
 
 export const configMiddlewares = (express, app) => {
 
@@ -8,6 +9,8 @@ export const configMiddlewares = (express, app) => {
   // other moddlewares config and init
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
+  // parse cookies
+  app.use(cookiesParser);
   
   // routes as middlewares
   app.use(authRouter);
