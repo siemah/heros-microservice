e9685d25505bf372ebd765d69675912a55ecbb43
siemah <m_hamiche@inptic.edu.dz>
2019-09-25 16:07:51 +0100

    render helmet tags

--- booksapp/web app/src/server/utils/tools.js
+++ booksapp/web app/src/server/utils/tools.js
@@ -5,16 +5,19 @@ import serialize from 'serialize-javascript';
  * @param {string} markup html tag retrieved fron jsx
  * @param {object} data contain details about app
  */
-export const jsxToHtml = (markup, data={}) => (`
+export const jsxToHtml = (markup, helmet, data={}) => (`
   <!DOCTYPE html>
-  <html>
+  <html ${helmet.htmlAttributes.toString()}>
     <head>
       <!-- meta and title tags -->
-      <script src="/bundle.js" defer></script>
-      <script>window.__INITIAL_DATA__ = ${serialize(data)}</script>
+      ${helmet.title.toString()}
+      ${helmet.meta.toString()}
+      ${helmet.link.toString()}
     </head>
     <body>
       <div id="app">${markup}</div>
+      <script src="/bundle.js" defer></script>
+      <script>window.__INITIAL_DATA__ = ${serialize(data)}</script>
     </body>
   </html>
 `);
