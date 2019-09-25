cb3e8c20ae6edb44c3e9d2b05fb880f3224bebf3
siemah <m_hamiche@inptic.edu.dz>
2019-09-25 19:39:17 +0100

    implement AuthContext for auth global state

--- booksapp/web app/src/shared/components/App.js
+++ booksapp/web app/src/shared/components/App.js
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, { useState, } from 'react';
 import { Route, Switch, } from 'react-router-dom'
 import routes from './routes/config';
 import PrivateRoute from './routes/PrivateRoute';
 import GuestRoute from './routes/GuestRoute';
 import NoMatch from '../components/pages/NoMatch';
 import HeaderMenu from './widgets/HeaderMenu';
+import { AuthProvider, authContextDefaultValue } from './context/auth';
 
-const App = () => {
+const App = (props) => {
+  // authentication state of user
+  const [auth, setAuth] = useState(authContextDefaultValue);
+  // main route include guest & private routes
   const _mainRoutes = routes.map(({ path, component: C, exact, mode = 'none', ...rest }) => {
     switch (mode) {
       case 'private':
@@ -14,16 +18,19 @@ const App = () => {
       case 'guest':
         return <GuestRoute key={path} {...rest} exact={exact} path={path} component={C} />
       default:
-        return <Route key={path} exact={exact} path={path} {...rest} render={props => <C {...rest} {...props}/>} />
+        return <Route key={path} exact={exact} path={path} {...rest} render={props => <C {...rest} {...props} />} />
     }
-  })
+  });
+
   return (
     <div>
-      <HeaderMenu />
-      <Switch>
-        {_mainRoutes}
-        <Route component={NoMatch} />
-      </Switch>
+      <AuthProvider value={{ user: auth, setAuth, }}>
+        <HeaderMenu />
+        <Switch>
+          {_mainRoutes}
+          <Route component={NoMatch} />
+        </Switch>
+      </AuthProvider>
     </div>
   )
 }
