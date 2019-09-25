5a76e53cb4de243f242974a40135f5c2f8ce8125
siemah <m_hamiche@inptic.edu.dz>
2019-09-25 14:58:42 +0100

    404 page

--- booksapp/web app/src/shared/components/App.js
+++ booksapp/web app/src/shared/components/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import { Route, Switch, Redirect, } from 'react-router-dom'
+import { Route, Switch, } from 'react-router-dom'
 import routes from './routes/config';
 import PrivateRoute from './routes/PrivateRoute';
 import GuestRoute from './routes/GuestRoute';
+import NoMatch from '../components/pages/NoMatch';
 
 const App = () => {
   const _mainRoutes = routes.map(({ path, component: C, exact, mode = 'none', ...rest }) => {
@@ -19,6 +20,7 @@ const App = () => {
     <div>
       <Switch>
         {_mainRoutes}
+        <Route component={NoMatch} />
       </Switch>
     </div>
   )
---
+++ booksapp/web app/src/shared/components/pages/NoMatch.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NoMatch = () => {
+  return (
+    <H1>
+      404, Not Found :(
+    </H1>
+  )
+}
+
+export default NoMatch;
