1a4fbd578b79a65832b00efb2a107b659efd9696
siemah <m_hamiche@inptic.edu.dz>
2019-09-25 19:37:14 +0100

    handle login at server side

---
+++ booksapp/web app/src/server/routes/auth.js
@@ -0,0 +1,25 @@
+import { Router, } from 'express';
+import { postLogin } from '../../shared/services/auth';
+import endpoints from '../config/endpoints'
+
+let authRouter = Router();
+
+authRouter.post(
+  '/auth/login',
+  (req, res) => {
+    let { email = null, password = null } = req.body;
+    if (!email || !password)
+      return res.status(401).json({ message: 'Pair email/password must not be empty' });
+    else {
+      postLogin(endpoints.login, { email, password })
+        .then(_res => {
+          res.status(_res.status).json(_res);
+        })
+        .catch(error => {
+          res.status(400).json({message: 'Something went wrong, try again'});
+        })
+    }
+  }
+);
+
+export default authRouter;
