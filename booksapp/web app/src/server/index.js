91680883c289b9c375f1e2fe218a5e4a43458d26
siemah <m_hamiche@inptic.edu.dz>
2019-09-25 15:33:37 +0100

    serving static files from /public+jsx 2 html

--- booksapp/web app/src/server/index.js
+++ booksapp/web app/src/server/index.js
@@ -1,20 +1,23 @@
 import express from 'express';
 import React from 'react';
 import { renderToString, } from 'react-dom/server';
+import { StaticRouter, matchPath, } from 'react-router-dom';
+
+import routes from '../shared/components/routes/config';
+import { jsxToHtml } from './utils/tools';
 import App from '../shared/components/App';
-import { StaticRouter, } from 'react-router-dom'
 
 let app = express();
 
-app.use(express.static('/public'));
+app.use(express.static('public'));
 
 app.get('/*', (req, res) => {
   let _markup = renderToString(
-    <StaticRouter>
+    <StaticRouter location={req.url} context={{}}>
       <App />
     </StaticRouter>
   );
-  res.send(_markup);
+  res.send(jsxToHtml(_markup));
 });
 
 app.listen(3000, () => {
