d374bacf3f9f0b018ad8ba892b92b41101903fd8
siemah <m_hamiche@inptic.edu.dz>
2019-09-26 13:50:06 +0100

    handle rendering for loggedin users+ refactore

--- booksapp/web app/src/server/index.js
+++ booksapp/web app/src/server/index.js
@@ -1,10 +1,5 @@
 import express from 'express';
-import React from 'react';
-import { renderToString, } from 'react-dom/server';
-import { StaticRouter, matchPath, } from 'react-router-dom';
-import Helmet from 'react-helmet'
 import { jsxToHtml } from './utils/tools';
-import App from '../shared/components/App';
 import { configMiddlewares } from './middlewares';
 import User from './controllers/User';
 
@@ -13,17 +8,16 @@ let app = express();
 configMiddlewares(express, app);
 
 app.get('/*', (req, res) => {
-  let _markup = renderToString(
-    <StaticRouter location={req.url} context={{}}>
-      <App />
-    </StaticRouter>
-  );
-  let _helmet = Helmet.renderStatic();
   // retrieve user data depend on cookies sended
   let user = new User();
   user.getUserData(req.cookies._auth)
-    .then(userData => res.send(jsxToHtml(_markup, _helmet, { user: userData, })))
-    .catch(() => res.send(jsxToHtml(_markup, _helmet, { user: {}, })));
+    .then(userData => {
+      let _render = jsxToHtml(req.url, { user: userData, }, { user: userData, })
+      if(typeof _render === 'object')
+        res.redirect(_render.redirectTo)
+      else res.send(_render);
+    })
+    .catch(() => res.send(jsxToHtml(req.url, { user: {}, }, { user: {}, })));
 });
 
 app.listen(3000, () => {
