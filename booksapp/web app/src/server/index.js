266a11034d688191eefeb0754f4b8e8059c284f9
siemah <m_hamiche@inptic.edu.dz>
2019-09-26 18:26:38 +0100

    retrieve data depend on link visited via user

--- booksapp/web app/src/server/index.js
+++ booksapp/web app/src/server/index.js
@@ -1,23 +1,34 @@
 import express from 'express';
+import { matchPath, } from 'react-router-dom'
 import { jsxToHtml } from './utils/tools';
 import { configMiddlewares } from './middlewares';
 import User from './controllers/User';
+import routes from '../shared/components/routes/config';
 
 let app = express();
 
 configMiddlewares(express, app);
 
 app.get('/*', (req, res) => {
-  // retrieve user data depend on cookies sended
+  let _activeRoute = routes.find(route => matchPath(req.url, route)) || {};
+  let _fetchInitialData = _activeRoute.fetchInitialData
+    ? _activeRoute.fetchInitialData(req.url)
+    : Promise.resolve();
+  // and get initial data to render current page
   let user = new User();
-  user.getUserData(req.cookies._auth)
-    .then(userData => {
-      let _render = jsxToHtml(req.url, { user: userData, }, { user: userData, })
-      if(typeof _render === 'object')
+  Promise.all([
+    user.getUserData(req.cookies._auth),// retrieve user data depend on cookies sended
+    _fetchInitialData,
+  ])
+    .then(([userData, initialData = []]) => {
+      let _render = jsxToHtml(req.url, { user: userData, data: initialData }, { user: userData, data: initialData })
+      console.log('render', typeof _render=== 'string')
+      if (typeof _render === 'string')
+        res.send(_render);
+      else
         res.redirect(_render.redirectTo)
-      else res.send(_render);
     })
-    .catch(() => res.send(jsxToHtml(req.url, { user: {}, }, { user: {}, })));
+    .catch(() => res.send(jsxToHtml(req.url, { user: {}, data: [] }, { user: {}, data: [] })));
 });
 
 app.listen(3000, () => {
