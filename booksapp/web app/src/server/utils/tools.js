12783acb9a6595cb587cd918cbec16969d8e69d3
siemah <m_hamiche@inptic.edu.dz>
2019-09-26 13:47:52 +0100

    return html string from JSX/object for redirection

--- booksapp/web app/src/server/utils/tools.js
+++ booksapp/web app/src/server/utils/tools.js
@@ -1,23 +1,42 @@
 import serialize from 'serialize-javascript';
+import React from 'react';
+import { renderToString, } from 'react-dom/server';
+import { StaticRouter, } from 'react-router-dom';
+import Helmet from 'react-helmet';
+import App from '../../shared/components/App';
 
 /**
  * render a html page based on jsx
- * @param {string} markup html tag retrieved fron jsx
- * @param {object} data contain details about app
+ * @param {string} location the current link hit by user
+ * @param {object} context data 2 send for user needed by app
+ * @param {object} data contain details about app most of time like context
+ * @return string which is html tags or 
+ *  object with redirecTo prop in case detecte redirection like private routes 
  */
-export const jsxToHtml = (markup, helmet, data={}) => (`
-  <!DOCTYPE html>
-  <html ${helmet.htmlAttributes.toString()}>
-    <head>
-      <!-- meta and title tags -->
-      ${helmet.title.toString()}
-      ${helmet.meta.toString()}
-      ${helmet.link.toString()}
-    </head>
-    <body>
-      <div id="app">${markup}</div>
-      <script src="/bundle.js" defer></script>
-      <script>window.__INITIAL_DATA__ = ${serialize(data)}</script>
-    </body>
-  </html>
-`);
+export const jsxToHtml = (location, context, data) => {
+  let _markup = renderToString(
+    <StaticRouter location={location} context={context}>
+      <App />
+    </StaticRouter>
+  );
+  if(context.url) return {
+    redirectTo: context.url,
+  }
+  let _helmet = Helmet.renderStatic();
+  return (`
+    <!DOCTYPE html>
+    <html ${_helmet.htmlAttributes.toString()}>
+      <head>
+        <!-- meta and title tags -->
+        ${_helmet.title.toString()}
+        ${_helmet.meta.toString()}
+        ${_helmet.link.toString()}
+      </head>
+      <body>
+        <div id="app">${_markup}</div>
+        <script src="/bundle.js" defer></script>
+        <script>window.__INITIAL_DATA__ = ${serialize(data)}</script>
+      </body>
+    </html>
+  `);
+}
