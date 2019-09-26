ac97bd4726fa92ca532fc2706531d9076116920f
siemah <m_hamiche@inptic.edu.dz>
2019-09-26 15:13:59 +0100

    render user loggedin name or login page link

--- booksapp/web app/src/shared/components/widgets/HeaderMenu.js
+++ booksapp/web app/src/shared/components/widgets/HeaderMenu.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import { NavLink, } from 'react-router-dom';
 import routes from '../routes/config'
+import AuthContext from '../context/auth';
 
 const HeaderMenu = () => {
+  const _authContext = React.useContext(AuthContext);
   const linksList = routes.map(({path}) => (
+    !path.includes('login') &&
     <li key={path}><NavLink to={path}>{path}</NavLink></li>
-  ))
+  ));
   return (
     <nav>
       <ul>
+        <li>
+          <NavLink to={'/login'}>
+          {
+            !!_authContext.user.fullname 
+              ? _authContext.user.fullname
+              : 'Login'
+          }
+          </NavLink>
+        </li>
         { linksList }
       </ul>
     </nav>
