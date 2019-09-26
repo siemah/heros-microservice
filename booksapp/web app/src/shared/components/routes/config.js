89c4d7c19cc6d56bf1dab5d8c4c7862b76ca5ebe
siemah <m_hamiche@inptic.edu.dz>
2019-09-26 18:16:56 +0100

    add books route

--- booksapp/web app/src/shared/components/routes/config.js
+++ booksapp/web app/src/shared/components/routes/config.js
@@ -1,8 +1,10 @@
 import Home from '../pages/Home';
 import Login from '../pages/Login';
 import { postLogin, } from '../../services/auth'
+import Books from '../pages/Books';
+import { fetchBooks } from '../../services/book';
 
-export default [
+const routes = [
   {
     path: '/',
     exact: true,
@@ -10,9 +12,18 @@ export default [
   },
   {
     path: '/login',
-    exact: true,
     mode: 'guest',
     component: Login,
     postLogin: postLogin
   },
+  {
+    path: '/books',
+    mode: 'private',
+    component: Books,
+    fetchInitialData: (path=null) => {
+      return fetchBooks(path.split('/').pop())
+    },
+  },
 ];
+
+export default routes;
