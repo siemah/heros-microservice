559dbd888cecc18747b507dc7811ad6b4ba3f040
siemah <m_hamiche@inptic.edu.dz>
2019-09-27 16:49:56 +0100

    passe opts param 2 add options for fetch

--- booksapp/web app/src/shared/services/book.js
+++ booksapp/web app/src/shared/services/book.js
@@ -3,14 +3,16 @@ import endpoints from "../config/endpoints";
 /**
  * retrieve a list of all books
  * @param {string} book mayeb slug or book id
+ * @param {object} opts list of options of fetch
+ * @return Promise<object|null> contain list of books
  */
-export const fetchBooks = (book) => {
+export const fetchBooks = (book, opts={}) => {
   const endpoint = book !== 'books'
     ? `${endpoints.book}/${book}` 
     : `${endpoints.booksList}`;
   const encodedURI = encodeURI(endpoint)
 
-  return fetch(encodedURI)
+  return fetch(encodedURI, opts)
     .then((data) => data.json())
     .then((books) => {
       if(!!book) return books 
