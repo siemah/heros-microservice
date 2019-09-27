826a85a9a3142c6ed27de179cf57fa3380df57cc
siemah <m_hamiche@inptic.edu.dz>
2019-09-27 19:08:26 +0100

    logout user route & erase all cookies

--- booksapp/web app/src/server/routes/auth.js
+++ booksapp/web app/src/server/routes/auth.js
@@ -14,7 +14,7 @@ authRouter.post(
       postLogin(endpoints.login, { email, password })
         .then(_res => {
           if(_res.status === 200) 
-            res.cookie('_auth', _res.user.token);
+            res.cookie('_auth', _res.user.token, { expires: new Date(Date.now() + 1000 * 3600 * 7) , httpOnly: true, });
           res.status(_res.status).json(_res);
         })
         .catch(error => {
@@ -23,5 +23,13 @@ authRouter.post(
     }
   }
 );
+
+authRouter.get(
+  '/auth/logout',
+  (req, res) => {
+    res.cookie('_auth', null,  { expires: new Date(Date.now() - 900000) });
+    res.redirect('/login');
+  }
+)
 
 export default authRouter;
