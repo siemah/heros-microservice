c208a972932b92a55554ae1a07944c173402afa8
siemah <m_hamiche@inptic.edu.dz>
2019-09-28 17:33:44 +0100

    render data or default value

--- booksapp/web app/src/shared/utils/data.js
+++ booksapp/web app/src/shared/utils/data.js
@@ -2,10 +2,10 @@ export const getDataFromWindowOrContext = (name=null, staticContext, defaultValu
   let _data = defaultValue;
   if (__isBrowser__) {
     // console.log(window.__INITIAL_DATA__)
-    _data = window.__INITIAL_DATA__ || [];
+    _data = window.__INITIAL_DATA__ || defaultValue;
     delete window.__INITIAL_DATA__;
   } else {
-    _data = staticContext.data || [];
+    _data = staticContext.data || defaultValue;
   }
   return _data;
 };
