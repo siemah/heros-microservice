ebe90701b05f1eee5ef486ced1c69b3e618a948a
siemah <m_hamiche@inptic.edu.dz>
2019-09-28 17:35:50 +0100

    refactore & assign null values rather than {}

--- booksapp/web app/src/server/index.js
+++ booksapp/web app/src/server/index.js
@@ -10,18 +10,19 @@ let app = express();
 configMiddlewares(express, app);
 
 app.get('/*', (req, res) => {
+  let _userToken = req.cookies._auth;
   let _activeRoute = routes.find(route => matchPath(req.url, route)) || {};
   let _fetchInitialData = _activeRoute.fetchInitialData
-    ? _activeRoute.fetchInitialData(req.url)
-    : Promise.resolve();
+    ? _activeRoute.fetchInitialData(req.url, { headers: {Authorization: `JWT ${_userToken}`}})
+    : Promise.resolve(null);
   // and get initial data to render current page
   let user = new User();
   Promise.all([
-    user.getUserData(req.cookies._auth),// retrieve user data depend on cookies sended
+    user.getUserData(_userToken),// retrieve user data depend on cookies sended
     _fetchInitialData,
   ])
-    .then(([userData, initialData = []]) => {
-      let _render = jsxToHtml(req.url, { user: userData, data: initialData }, { user: userData, data: initialData })
+    .then(([userData, initialData]) => {
+      let _render = jsxToHtml(req.url, { user: userData, data: initialData })
 
       if (typeof _render === 'string')
         res.send(_render);
@@ -29,7 +30,7 @@ app.get('/*', (req, res) => {
         res.redirect(_render.redirectTo)
     })
     .catch(() => {
-      let _render = jsxToHtml(req.url, { user: {}, data: [] }, { user: {}, data: [] });
+      let _render = jsxToHtml(req.url, { user: null, data: null });
 
       if (typeof _render === 'string')
         res.send(_render);
