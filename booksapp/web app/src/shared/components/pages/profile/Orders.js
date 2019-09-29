ff9d0df7e03d37c7c3ba53fa5ce9630ab6823ed3
siemah <m_hamiche@inptic.edu.dz>
2019-09-29 18:25:53 +0100

    fetch orders on client side

--- booksapp/web app/src/shared/components/pages/profile/Orders.js
+++ booksapp/web app/src/shared/components/pages/profile/Orders.js
@@ -1,16 +1,39 @@
-import React, { useContext, useState, } from 'react'
+import React, { useContext, useState, useEffect,  } from 'react'
 import SEO from '../../widgets/SEO';
 import AuthContext from '../../context/auth';
 import { getDataFromWindowOrContext } from '../../../utils/data';
 
-const Orders = ({ staticContext, }) => {
+const Orders = ({ staticContext, fetchInitialData, }) => {
   const _authContext = useContext(AuthContext);
   const _orders = getDataFromWindowOrContext('orders', staticContext, []);
   const [orders, setOrders] = useState({
     orders: _orders.orders,
     loading: false,
   });
-  console.log(orders.orders);
+
+  useEffect(() => {
+    let _isMount = true;
+    const _fetchData = async () => {
+      let opts = {
+        headers: {
+          Authorization: `JWT ${_authContext.user.token}`,
+        }
+      }
+      try {
+        console.log(` fetch ${!orders.orders && 'ok'}`)
+        let _data = await fetchInitialData(null, opts);
+        if( _data.orders )
+          _isMount && setOrders({...orders, orders: _data.orders}) 
+      } catch (error) {
+        _isMount && setOrders({...orders, message: 'Something went wrong'}) 
+      }
+    }
+    !orders.orders && _fetchData();
+    
+    return () => {
+    _isMount = false;
+    };
+  }, []);
   return (
     <div>
       <SEO title={`${_authContext.user.fullname} Orders`} />
@@ -18,7 +41,7 @@ const Orders = ({ staticContext, }) => {
       <nav>
         <ul>
           {
-            orders.orders.map( ({ _id, book, createdAt, dueDate, isCanceled}) => (
+            orders.orders && orders.orders.map( ({ _id, book, createdAt, dueDate, isCanceled}) => (
               <li key={_id}>
                 order of book {book} at {createdAt}<br />
                 {!isCanceled && <button>cancel</button>}
