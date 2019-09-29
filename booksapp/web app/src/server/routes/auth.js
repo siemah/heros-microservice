0539f16649bce8d9e4d845ed5ddfe19d809e6915
siemah <m_hamiche@inptic.edu.dz>
2019-09-29 13:57:22 +0100

    register route

--- booksapp/web app/src/server/routes/auth.js
+++ booksapp/web app/src/server/routes/auth.js
@@ -2,8 +2,29 @@ import { Router, } from 'express';
 import { postLogin } from '../../shared/services/auth';
 import endpoints from '../config/endpoints'
 import cookiesConfig from '../config/cookies';
+import User from '../controllers/User';
 
 let authRouter = Router();
+
+authRouter.post(
+  '/auth/register',
+  async (req, res) => {
+    let { email = null, password = null, fname = null, lname = null } = req.body;
+    if (!email || !password || !fname || !lname)
+      return res.status(401).json({ message: 'All fields are required' });
+    else {
+      try {
+        let user = new User(fname, lname, email, password);
+        let _response = await user.create();
+        if(_response.status === 'OK') 
+          res.cookie('_auth', _response.user.token, cookiesConfig.options);
+        res.status(_response.status === 'OK'? 201 : 401).json(_response);
+      } catch (error) {
+        res.status(401).json({ status:'NO',message: 'Something went wrong try after a moment' });
+      }
+    }
+  }
+);
 
 authRouter.post(
   '/auth/login',
