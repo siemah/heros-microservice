8910aee6f1d2ec6d991b6f46a853342c7277bccd
siemah <m_hamiche@inptic.edu.dz>
2019-09-29 18:01:49 +0100

    add orders of user endpoint

--- booksapp/web app/src/shared/config/endpoints.js
+++ booksapp/web app/src/shared/config/endpoints.js
@@ -3,6 +3,7 @@ const endpoints = {
   booksList: `http://localhost:3001/books`,
   book: `http://localhost:3001/book`,
   order: `http://localhost:3003/order`,
+  orders: `http://localhost:3003/orders/me`,
 };
 
 export default endpoints;
