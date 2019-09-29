242b81679930b85277bc043a61fceb830de708ad
siemah <m_hamiche@inptic.edu.dz>
2019-09-29 18:03:05 +0100

    fetch orders + post new ordser

---
+++ booksapp/web app/src/shared/services/orders.js
@@ -0,0 +1,36 @@
+import endpoints from "../config/endpoints";
+
+/**
+ * send a requst to create new order
+ * @param {object} order contain book id 
+ * @param {string} token loggedin user jwt
+ */
+export const postOrder = async (order, token) => {
+  try {
+    let res = await fetch(
+      endpoints.order, 
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `JWT ${token}`,
+        },
+        body: JSON.stringify(order),
+      });
+    await res.json();
+  } catch (error) {
+    throw new Error('Something went wrong');
+  }
+}
+
+export const fetchOrder = async (location, opts={}) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      let _res = await fetch(endpoints.orders, opts);
+      let _data = await _res.json();
+      resolve(_data)
+    } catch (error) {
+      reject([]);
+    }
+  });
+}
