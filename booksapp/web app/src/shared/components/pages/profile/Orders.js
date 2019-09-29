e89ed267c46c2dfbafe379d459162b7e67da0b34
siemah <m_hamiche@inptic.edu.dz>
2019-09-29 18:03:34 +0100

    display all orders

--- booksapp/web app/src/shared/components/pages/profile/Orders.js
+++ booksapp/web app/src/shared/components/pages/profile/Orders.js
@@ -1,17 +1,30 @@
-import React, { useContext, } from 'react'
+import React, { useContext, useState, } from 'react'
 import SEO from '../../widgets/SEO';
 import AuthContext from '../../context/auth';
+import { getDataFromWindowOrContext } from '../../../utils/data';
 
-const Orders = () => {
+const Orders = ({ staticContext, }) => {
   const _authContext = useContext(AuthContext);
-
+  const _orders = getDataFromWindowOrContext('orders', staticContext, []);
+  const [orders, setOrders] = useState({
+    orders: _orders.orders,
+    loading: false,
+  });
+  console.log(orders.orders);
   return (
     <div>
       <SEO title={`${_authContext.user.fullname} Orders`} />
       <h1>My Orders</h1>
       <nav>
         <ul>
-          {/** list of orders with a possibility for cancelation */}
+          {
+            orders.orders.map( ({ _id, book, createdAt, dueDate, isCanceled}) => (
+              <li key={_id}>
+                order of book {book} at {createdAt}<br />
+                {!isCanceled && <button>cancel</button>}
+              </li>
+            ))
+          }
         </ul>
       </nav>
     </div>
