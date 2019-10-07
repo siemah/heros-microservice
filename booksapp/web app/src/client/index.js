2e938440938af410ccc51ce432f080b3a5bd8adf
siemah <m_hamiche@inptic.edu.dz>
2019-10-07 15:42:03 +0100

    setup isomorphic for client rendering

--- booksapp/web app/src/client/index.js
+++ booksapp/web app/src/client/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import { hydrate } from 'react-dom';
 import App from '../shared/components/App';
-import { BrowserRouter as Router } from 'react-router-dom'
+import { BrowserRouter as Router } from 'react-router-dom';
+import StyleContext from 'isomorphic-style-loader/StyleContext';
+
+const insertCss = (...styles) => {
+  const removeCss = styles.map(style => style._insertCss())
+  return () => removeCss.forEach(dispose => dispose())
+}
 
 hydrate(
   <Router>
-    <App />
+    <StyleContext.Provider value={{ insertCss }}>
+      <App />
+    </StyleContext.Provider>
   </Router>,
   document.getElementById('app')
 );
