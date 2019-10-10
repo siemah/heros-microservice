cd943e0e0083157f60f40d48a227067d23504645
siemah <m_hamiche@inptic.edu.dz>
2019-10-10 23:18:28 +0100

    move common module 2 shared config constant

--- booksapp/web app/webpack.config.js
+++ booksapp/web app/webpack.config.js
@@ -13,64 +13,6 @@ const config = {
     publicPath: '/',
     sourcePrefix: '  ',
   },
-}
-
-var browserConfig = {
-  mode: 'development',
-  entry: ['@babel/polyfill', './client/index.js'],
-  output: {
-    publicPath: '/',
-    filename: DEBUG ? '[name].js?[chunkhash]' : '[name].[chunkhash].js',
-    chunkFilename: DEBUG ? '[name].[id].js?[chunkhash]' : '[name].[id].[chunkhash].js',
-  },
-
-  target: 'web',
-
-  module: {
-    rules: [
-      { test: /\.(js)$/, use: 'babel-loader' },
-      {
-        test: /\.css$/,
-        use: [
-          'isomorphic-style-loader',
-          {
-            loader: 'css-loader',
-            options: {
-              importLoaders: 1
-            }
-          },
-          'postcss-loader'
-        ]
-      },
-      {
-        test: /\.(png|jpg|jpeg|gif|svg|woff|woff2)$/,
-        loader: 'url-loader',
-        query: {
-          name: '[name]-[hash].[ext]',
-          limit: 10000,
-        },
-      },
-    ]
-  },
-  plugins: [
-    new webpack.DefinePlugin({
-      __isBrowser__: "true"
-    }),
-  ]
-}
-
-var serverConfig = {
-  mode: 'development',
-  entry: ['@babel/polyfill', './server/index.js'],
-  externals: [nodeExternals()],
-  output: {
-    filename: '../server.js',
-    libraryTarget: 'commonjs2',
-    publicPath: '/',
-    // path: '/build/',
-  },
-
-  target: 'node',
   module: {
     rules: [
       { test: /\.(js)$/, use: 'babel-loader' },
@@ -104,6 +46,38 @@ var serverConfig = {
       // },
     ]
   },
+}
+
+var browserConfig = {
+  mode: 'development',
+  entry: ['@babel/polyfill', './client/index.js'],
+  output: {
+    publicPath: '/',
+    filename: DEBUG ? '[name].js?[chunkhash]' : '[name].[chunkhash].js',
+    chunkFilename: DEBUG ? '[name].[id].js?[chunkhash]' : '[name].[id].[chunkhash].js',
+  },
+
+  target: 'web',
+
+  plugins: [
+    new webpack.DefinePlugin({
+      __isBrowser__: "true"
+    }),
+  ]
+}
+
+var serverConfig = {
+  mode: 'development',
+  entry: ['@babel/polyfill', './server/index.js'],
+  externals: [nodeExternals()],
+  output: {
+    filename: '../server.js',
+    libraryTarget: 'commonjs2',
+    publicPath: '/',
+    // path: '/build/',
+  },
+
+  target: 'node',
   plugins: [
     new webpack.DefinePlugin({
       __isBrowser__: "false"
